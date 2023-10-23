332cc09b21a35609ee2b03c626b27899cdc15909
Raphael Moraes <raphael.moraes@niduu.com>
2023-10-23 17:21:19 -0300

    refactor(realty): remove create method and change creation to constructor

--- src/realty/domain/realty.spec.ts
+++ src/realty/domain/realty.spec.ts
@@ -4,10 +4,11 @@ describe('Realty', () => {
   it('should throw an error when title length is less than 3 characters', () => {
     const expected_error = 'title must contain between 3 and 300 characters';
     const shortTitle = 'ab';
-    expect(() =>
-      Realty.create({
-        title: shortTitle,
-      }),
+    expect(
+      () =>
+        new Realty({
+          title: shortTitle,
+        }),
     ).toThrowError(expected_error);
   });
 
@@ -15,20 +16,22 @@ describe('Realty', () => {
     const expected_error = 'title must contain between 3 and 300 characters';
     const longTitle =
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec elit et arcu malesuada porttitor vitae eleifend augue. Proin efficitur elit nunc, fermentum gravida elit eleifend ut. Donec bibendum maximus risus, quis aliquet nisl fermentum at. Nulla volutpat venenatis orci non venenatis. Sed at ligula vel dui faucibus auctor ac in nulla. Nam ut augue quis urna sodales vulputate. Maecenas efficitur, neque nec pulvinar volutpat, erat turpis blandit tellus, at malesuada diam felis eu tortor..';
-    expect(() =>
-      Realty.create({
-        title: longTitle,
-      }),
+    expect(
+      () =>
+        new Realty({
+          title: longTitle,
+        }),
     ).toThrowError(expected_error);
   });
 
   it('should throw an error when title is null', () => {
     const expected_error = 'title is required';
     const nullTitle = null;
-    expect(() =>
-      Realty.create({
-        title: nullTitle,
-      }),
+    expect(
+      () =>
+        new Realty({
+          title: nullTitle,
+        }),
     ).toThrowError(expected_error);
   });
 
@@ -37,17 +40,18 @@ describe('Realty', () => {
       'description must be less than or equal to 500 characters';
     const longDescription =
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec elit et arcu malesuada porttitor vitae eleifend augue. Proin efficitur elit nunc, fermentum gravida elit eleifend ut. Donec bibendum maximus risus, quis aliquet nisl fermentum at. Nulla volutpat venenatis orci non venenatis. Sed at ligula vel dui faucibus auctor ac in nulla. Nam ut augue quis urna sodales vulputate. Maecenas efficitur, neque nec pulvinar volutpat, erat turpis blandit tellus, at malesuada diam felis eu tortor..';
-    expect(() =>
-      Realty.create({
-        title: 'some title',
-        description: longDescription,
-      }),
+    expect(
+      () =>
+        new Realty({
+          title: 'some title',
+          description: longDescription,
+        }),
     ).toThrowError(expected_error);
   });
 
   it('should return description null when description is null', () => {
     const nullDescription = null;
-    const realty = Realty.create({
+    const realty = new Realty({
       title: 'some title',
       description: nullDescription,
     });
@@ -56,27 +60,29 @@ describe('Realty', () => {
 
   it('should throw an error when price is less than 0', () => {
     const invalidPrice = -100;
-    expect(() =>
-      Realty.create({
-        title: 'some title',
-        price: invalidPrice,
-      }),
+    expect(
+      () =>
+        new Realty({
+          title: 'some title',
+          price: invalidPrice,
+        }),
     ).toThrowError('price must be greater than 0');
   });
 
   it('should throw an error when price is 0', () => {
     const invalidPrice = 0;
-    expect(() =>
-      Realty.create({
-        title: 'some title',
-        price: invalidPrice,
-      }),
+    expect(
+      () =>
+        new Realty({
+          title: 'some title',
+          price: invalidPrice,
+        }),
     ).toThrowError('price must be greater than 0');
   });
 
   it('should return price null when price is null', () => {
     const nullPrice = null;
-    const realty = Realty.create({
+    const realty = new Realty({
       title: 'some title',
       price: nullPrice,
     });
@@ -90,7 +96,7 @@ describe('Realty', () => {
       price: 150000,
     };
 
-    const realty = Realty.create({
+    const realty = new Realty({
       title: expected_value.title,
       description: expected_value.description,
       price: expected_value.price,
--- src/realty/domain/realty.ts
+++ src/realty/domain/realty.ts
@@ -10,42 +10,45 @@ export class Realty {
   private _description: string;
   private _price: number;
 
-  private constructor(props: RealtyProps) {
-    this._title = props.title;
-    this._description = props.description;
-    this._price = props.price;
+  constructor(props: RealtyProps) {
+    this.title = props.title;
+    this.description = props.description;
+    this.price = props.price;
   }
 
-  public static create(props: RealtyProps) {
-    if (!props.title) {
+  private set title(value: string) {
+    if (!value) {
       throw new Error('title is required');
     }
-
-    if (props.title.length < 3 || props.title.length > 300) {
+    if (value.length < 3 || value.length > 300) {
       throw new Error('title must contain between 3 and 300 characters');
     }
-
-    if (props.description?.length > 500) {
-      throw new Error(
-        'description must be less than or equal to 500 characters',
-      );
-    }
-
-    if (props.price != null && props.price <= 0) {
-      throw new Error('price must be greater than 0');
-    }
-
-    return new Realty(props);
+    this._title = value;
   }
 
   get title(): string {
     return this._title;
   }
 
+  private set description(value: string) {
+    if (value && value.length > 500) {
+      throw new Error(
+        'description must be less than or equal to 500 characters',
+      );
+    }
+    this._description = value;
+  }
+
   get description(): string {
     return this._description;
   }
 
+  private set price(value: number) {
+    if (value != null && value <= 0) {
+      throw new Error('price must be greater than 0');
+    }
+    this._price = value;
+  }
   get price(): number {
     return this._price;
   }
